0cfbad6ba1435f95e668dce105284cda5c2d3b1c
DenisSob74 <sobolevden51@gmail.com>
2024-10-17 22:12:08 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Ведите строку: ")
+
+print("Вывод в верхнем регистре:", my_string .upper())
+print("Вывод в нижнем регистре:", my_string .lower())
+print("Вывод без пробелов", my_string.replace(" ",""))
+print("Вывод на экран первый символ строки:", my_string[0])
+print("Вывод на экран последнего симовола строки:", my_string[-1])
