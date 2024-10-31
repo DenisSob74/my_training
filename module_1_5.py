9fb1c6ee83b5c2a868688310eae3b259bc962b61
Денис <sobolevden51@gmail.com>
2024-10-31 18:04:37 +0500

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -3,6 +3,8 @@ print('immutable tuple: ', immutable_var)
 # immutable_var_1 = 1,2,3,4 # Ошибка типа: объект «кортеж»
 # immutable_var_1[0][0] = 2 # не поддерживает назначение элементов
 # print(immutable_var_1)
-mutable_list = [[1, 2, 3, 4, 5], 0]
-mutable_list[0][3] = "изменено"
+mutable_list = ('1', '2', '3', '4', '5', '0')
+mutable_list = list(mutable_list)
+mutable_list[5] = "изменено"
+mutable_list = tuple(mutable_list)
 print('Mutable list: ', mutable_list)
