5b949e5dcb4640f2ceac5d4cbedb3cb89e5beb4a
Денис <sobolevden51@gmail.com>
2024-11-11 22:31:04 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params(1, 25, [1, 2, 3])
+
+values_list = [1, 'строка', 34]
+values_dict = {'a': 1232, 'b': 3454, 'c': 'string'}
+print(print_params(*values_list))
+print(print_params(**values_dict))
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
