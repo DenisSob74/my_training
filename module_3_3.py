c141682b766ed9d36b1714ccb77245e17c4b2708
Денис <sobolevden51@gmail.com>
2024-11-11 22:33:14 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -6,8 +6,8 @@ print_params(1, 25, [1, 2, 3])
 
 values_list = [1, 'строка', 34]
 values_dict = {'a': 1232, 'b': 3454, 'c': 'string'}
-print(print_params(*values_list))
-print(print_params(**values_dict))
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
