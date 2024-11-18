3b210164a885b20f7130e611115409ff89f9e11c
Денис <sobolevden51@gmail.com>
2024-11-18 22:41:38 +0500

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,14 +1,14 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primers = []
 not_primers = []
-for number in range(2, len(numbers) + 1):
+for number in range(1, len(numbers) + 1):
     index = 0
     for is_prime in range(1, number + 1):
         if number % is_prime == 0:
             index += 1
     if index == 2:
         primers.append(is_prime)
-    else:
+    elif number > 1:
         not_primers.append(is_prime)
 
 print('primers = ', primers)
