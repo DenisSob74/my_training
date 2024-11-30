66aa028cfbfe0ff770c687bf5800f83eea26aeb7
Денис <sobolevden51@gmail.com>
2024-12-01 01:32:43 +0500

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,25 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        floor = 0
+        if floor >= new_floor or new_floor <= self.number_of_floor:
+            print(f'В {self.name} {self.number_of_floor} этажа(-ей)')
+            for floor in range(new_floor):
+                print(floor + 1)
+            print(f'поднимаемся на {new_floor} этаж')
+        else:
+            print(f'В доме "{self.name}" "Такого этажа не существует"')
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
