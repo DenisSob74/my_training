1dafa4838159375a809fbe53fcec45d0e05dfcd9
Денис <sobolevden51@gmail.com>
2024-12-10 00:31:40 +0500

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -1,13 +1,17 @@
 class House:
     houses_history = []
+
     def __new__(cls, *args, **kwargs):
         cls.houses_history.append(args[0])
         return super().__new__(cls)
-    def __int__(self, name, number_of_floor):
+
+    def __init__(self, name, number_of_floor):
         self.name = name
         self.number_of_floor = number_of_floor
+
     def __del__(self):
-        print(f'{self.houses_history} снесён, но он останется в истории')
+        print(self.name, "снесён, но он останется в истории")
+
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
 h2 = House('ЖК Акация', 20)
