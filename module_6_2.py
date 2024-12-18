633dc809320122cb9cf31a06b42175d5a6402f70
Денис <sobolevden51@gmail.com>
2024-12-18 21:20:57 +0500

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,19 +1,20 @@
 class Vehicle:
-    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-    def __init__(self, owner, __model, __color, __engine_power):
-        self.owner = str(owner)
-        self.__model = str(__model)
-        self.__engine_power = int(__engine_power)
-        self.__color = str(__color)
+    __color_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner
+        self.model = model
+        self.engine_power = engine_power
+        self.color = color
 
     def get_model(self):
-        print(f"Модель: {self.__model}")
+        print(f"Модель: {self.model}")
 
     def get_horsepower(self):
-        print(f"Мощность двигателя: {self.__engine_power}")
+        print(f"Мощность двигателя: {self.engine_power}")
 
     def get_color(self):
-        print(f"Цвет: {self.__color}")
+        print(f"Цвет: {self.color}")
 
     def print_info(self):
         self.get_model()
@@ -22,13 +23,11 @@ class Vehicle:
         print(f'{self.owner} - владелец авто')
 
     def set_color(self, new_color):
-        self.new_color = str(new_color.lower())
-        for colors in __COLOR_VARIANTS:
-            colors = colors.lower()
-            if new_color in colors:
-                self.__color = new_color
-            else:
-                print(f"Нельзя сменить цвет на {self.new_color}")
+
+        if new_color.lower() in self.__color_VARIANTS:
+            self.color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
 
 
 class Sedan(Vehicle):
@@ -38,7 +37,7 @@ class Sedan(Vehicle):
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 vehicle1.print_info()
 
-#vehicle1.set_color('pink')
+vehicle1.set_color('pink')
 
 vehicle1.set_color('black')
 vehicle1.owner = 'Vasyok'
